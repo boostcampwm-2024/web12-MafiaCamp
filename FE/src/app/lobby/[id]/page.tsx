1276ad47062b7185f0db8249030104bd8b33e0fb
HyunJinNo <nhj9831@naver.com>
2024-11-02 23:39:31 +0900

    ui: 스크롤바 디자인 변경

--- FE/src/app/lobby/[id]/page.tsx
+++ FE/src/app/lobby/[id]/page.tsx
@@ -1,4 +1,5 @@
 import ChattingList from '@/components/lobby/room/ChattingList';
+import Link from 'next/link';
 
 export async function generateMetadata({
   params,
@@ -23,6 +24,12 @@ export default async function Page({
   return (
     <div className='flex flex-row items-center justify-between'>
       <h1 className='fixed left-4 top-4 text-5xl text-white'>{id}</h1>
+      <Link
+        className='fixed left-4 top-20 flex h-10 w-20 items-center justify-center rounded-full bg-white text-slate-800 hover:scale-105'
+        href='/lobby'
+      >
+        나가기
+      </Link>
       <ChattingList />
     </div>
   );
