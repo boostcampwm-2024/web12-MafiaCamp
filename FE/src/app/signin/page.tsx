8fe3eade4fc734012b5dfaa8a20bdc8735ddaffd
HyunJinNo <nhj9831@naver.com>
2024-11-02 19:14:22 +0900

    ui: 로그인 페이지 UI 구현

--- FE/src/app/signin/page.tsx
+++ FE/src/app/signin/page.tsx
@@ -1,3 +1,9 @@
+import SigninPanel from '@/components/signin/SigninPanel';
+
 export default function Page() {
-  return <div className='flex w-full flex-col items-center pt-20'></div>;
+  return (
+    <div className='flex w-full flex-col items-center pt-20'>
+      <SigninPanel />
+    </div>
+  );
 }
