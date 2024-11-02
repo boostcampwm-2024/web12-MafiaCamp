3c7ba5d7b096e83d556e5333acf570235fe8f4b3
HyunJinNo <nhj9831@naver.com>
2024-11-02 15:05:02 +0900

    ui: RoleInfo 컴포넌트 생성

--- FE/src/app/page.tsx
+++ FE/src/app/page.tsx
@@ -1,9 +1,11 @@
 import Introduction from '@/components/home/Introduction';
+import RoleInfo from '@/components/home/RoleInfo';
 
 export default function Home() {
   return (
-    <div>
+    <div className='flex flex-col gap-60'>
       <Introduction />
+      <RoleInfo />
     </div>
   );
 }
--- FE/src/components/home/Introduction.tsx
+++ FE/src/components/home/Introduction.tsx
@@ -8,11 +8,11 @@ const Introduction = () => {
   return (
     <div className='flex w-full flex-col items-center gap-8 pt-[25rem]'>
       <motion.h2
-        className='bg-gradient-to-r from-slate-400 to-white bg-clip-text text-5xl text-transparent'
+        className='bg-gradient-to-r from-slate-400 to-white bg-clip-text text-5xl font-bold text-transparent'
         initial={{ translateX: '-3rem', opacity: 0 }}
         whileInView={{ translateX: '0rem', opacity: 1 }}
         transition={{ duration: 0.5 }}
-        viewport={{ once: true, amount: 1 }}
+        viewport={{ once: true, amount: 0.9 }}
       >
         온라인으로 즐기는 마피아 게임
       </motion.h2>
@@ -21,7 +21,7 @@ const Introduction = () => {
         initial={{ translateX: '-3rem', opacity: 0 }}
         whileInView={{ translateX: '0rem', opacity: 1 }}
         transition={{ delay: 0.3, duration: 0.5 }}
-        viewport={{ once: true, amount: 1 }}
+        viewport={{ once: true, amount: 0.9 }}
       >
         화상 채팅으로 마피아 게임을 즐겨보세요!
       </motion.p>
@@ -31,7 +31,7 @@ const Introduction = () => {
           initial={{ rotate: '-90deg', opacity: 0 }}
           whileInView={{ rotate: '0deg', opacity: 1 }}
           transition={{ duration: 0.5 }}
-          viewport={{ once: true, amount: 0.8 }}
+          viewport={{ once: true, amount: 0.5 }}
         >
           <Lottie animationData={LottieFile} className='bg-transparent' />
         </motion.div>
@@ -40,7 +40,7 @@ const Introduction = () => {
           initial={{ translateX: '-1rem', opacity: 0 }}
           whileInView={{ translateX: '0rem', opacity: 1 }}
           transition={{ duration: 0.5 }}
-          viewport={{ once: true, amount: 1 }}
+          viewport={{ once: true, amount: 0.9 }}
         >
           <p>MafiaCamp는 여러 사람들과 실시간 화상 채팅을 통해</p>
           <p>마피아 게임을 즐길 수 있도록 지원합니다. 여러 사람들과의</p>
