0ae3623f0cc21920be2773fbbdc85d3df20847af
0Chord <seaweed.0chord@gmail.com>
2024-11-04 21:01:51 +0900

    feat: TypeORM 환경 설정 및 서버시작 시 TABLE 생성 SQL가 동작하게 구성

--- BE/src/app.module.ts
+++ BE/src/app.module.ts
@@ -1,10 +1,20 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { typeORMConfig } from './common/config/typeorm.config';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [ConfigModule.forRoot({
+    isGlobal: true,
+  }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) =>
+        typeORMConfig(configService),
+
+    }),
+  ],
 })
-export class AppModule {}
+export class AppModule {
+}
---
+++ BE/src/common/config/typeorm.config.ts
@@ -0,0 +1,28 @@
+import { DataSource, DataSourceOptions } from 'typeorm';
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { addTransactionalDataSource } from 'typeorm-transactional';
+import { ConfigService } from '@nestjs/config';
+
+
+export const typeOrmInfo = (configService: ConfigService): DataSourceOptions => ({
+  type: 'mysql' as const,
+  host: configService.get('DB_HOST'),
+  port: configService.get<number>('DB_PORT'),
+  username: configService.get('DB_USER'),
+  password: configService.get('DB_PASSWORD'),
+  database: configService.get('DB_NAME'),
+  entities: [],
+  synchronize: false,
+  logging: true,
+} as DataSourceOptions);
+
+export const typeORMConfig = async (configService: ConfigService): Promise<TypeOrmModuleOptions> => {
+  const ormInfo = typeOrmInfo(configService);
+  const dataSource = new DataSource(typeOrmInfo(configService));
+  await dataSource.initialize();
+
+  addTransactionalDataSource(dataSource);
+  return {
+    ...ormInfo,
+  } as TypeOrmModuleOptions;
+};
