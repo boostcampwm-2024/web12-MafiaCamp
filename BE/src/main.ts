857bec6fca065cc12feae2f769493d343ce65de0
simjaesung <tlawotjd4285@gmail.com>
2024-11-06 02:09:06 +0900

    feat: OpenVidu NestJS 환경설정 및 local ssl 인증키 발급

--- BE/src/main.ts
+++ BE/src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as fs from 'fs';
+import * as path from 'path';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  //process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0'; # Node.js 환경 변수로 SSL 검증 무시
+  const httpsOptions = {
+    key: fs.readFileSync(path.join(__dirname, '../ssl/localhost+2-key.pem')),
+    cert: fs.readFileSync(path.join(__dirname, '../ssl/localhost+2.pem')),
+  };
+
+  const app = await NestFactory.create(AppModule, { httpsOptions });
+  app.enableCors({
+    origin: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Accept', 'Authorization'],
+    credentials: true,
+  });
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
--- BE/src/openvidu/openvidu.service.ts
+++ BE/src/openvidu/openvidu.service.ts
@@ -1,14 +1,27 @@
 import { Injectable } from '@nestjs/common';
+import { OpenVidu } from 'openvidu-node-client';
 import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class OpenviduService {
-    private openViduUrl: string;
-    private openViduSecret: string;
+  private openvidu: OpenVidu;
 
-    constructor(private configService: ConfigService) {
-        this.openViduUrl = this.configService.get<string>('OPENVIDU_URL');
-        this.openViduSecret = this.configService.get<string>('OPENVIDU_SECRET');
+  constructor(private configService: ConfigService) {
+    const OPENVIDU_URL = this.configService.get<string>('OPENVIDU_URL');
+    const OPENVIDU_SECRET = this.configService.get<string>('OPENVIDU_SECRET');
+    console.log(OPENVIDU_URL, OPENVIDU_SECRET);
+
+    this.openvidu = new OpenVidu(OPENVIDU_URL, OPENVIDU_SECRET);
+  }
+
+  async testConnection() {
+    try {
+      await this.openvidu.fetch();
+      console.log('OpenVidu 서버에 연결되었습니다.');
+      return true;
+    } catch (error) {
+      console.error('OpenVidu 서버 연결 실패:', error);
+      return false;
     }
-
+  }
 }
