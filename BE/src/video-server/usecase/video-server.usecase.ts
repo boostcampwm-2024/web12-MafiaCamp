bd023d753dddc2d5d70ebcfa4bb1e513d18ed0fa
simjaesung <tlawotjd4285@gmail.com>
2024-11-07 09:09:51 +0900

    feat: video-server usecase interface 설정

--- BE/src/video-server/usecase/video-server.usecase.ts
+++ BE/src/video-server/usecase/video-server.usecase.ts
@@ -1,5 +1,16 @@
+import { OpenViduRole } from 'openvidu-node-client';
+import { SessionParticipant } from '../types/session-participant.type';
+
 export const VIDEO_SERVER_USECASE = Symbol.for('VIDEO_SERVER_USECASE');
 
 export interface VideoServerUsecase {
-  testConnection(): Promise<boolean>;
+  createSession(roomId: string): Promise<string>;
+  closeSession(roomId: string): Promise<void>;
+  generateToken(
+    roomId: string,
+    userId: string,
+    role: OpenViduRole,
+    nickname?: string,
+  ): Promise<string>;
+  getActiveParticipants(roomId: string): Promise<SessionParticipant[]>;
 }
