c29d6c9c0573680e89aa05bc6b4ac43938b8a4bd
simjaesung <tlawotjd4285@gmail.com>
2024-11-20 00:25:26 +0900

    feat: 커스텀 에러 생성 및 개선

--- BE/src/common/error/not.found.mafia.kill.log.exception.ts
+++ BE/src/common/error/not.found.mafia.kill.log.exception.ts
@@ -2,13 +2,13 @@ import { HttpException, HttpStatus } from '@nestjs/common';
 import { EXCEPTION_CODE } from './exception.code';
 import { EXCEPTION_MESSAGE } from './exception.message';
 
-export class UnauthorizedMafiaSelectException extends HttpException {
+export class NotFoundMafiaKillLogException extends HttpException {
   constructor() {
     super(
       {
-        code: EXCEPTION_CODE.UNAUTHORIZED_MAFIA_SELECT_EXCEPTION,
-        message: EXCEPTION_MESSAGE.UNAUTHORIZED_MAFIA_SELECT_EXCEPTION,
-        error: 'UnauthorizedMafiaSelectException',
+        code: EXCEPTION_CODE.NOT_FOUND_MAFIA_KILL_LOG,
+        message: EXCEPTION_MESSAGE.NOT_FOUND_MAFIA_KILL_LOG,
+        error: 'NotFoundMafiaKillLogException',
         statusCode: HttpStatus.BAD_REQUEST,
       },
       HttpStatus.BAD_REQUEST,
