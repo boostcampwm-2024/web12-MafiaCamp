d5b86217837b1f2356c323a188b9702c8d1c9107
0Chord <seaweed.0chord@gmail.com>
2024-11-22 16:23:49 +0900

    feat: event module에 필터 의존성 추가

--- BE/src/event/event.module.ts
+++ BE/src/event/event.module.ts
@@ -3,9 +3,15 @@ import { EventGateway } from './event.gateway';
 import { GameRoomModule } from 'src/game-room/game-room.module';
 import { EventManager } from './event-manager';
 import { GameModule } from 'src/game/game.module';
+import { APP_FILTER } from '@nestjs/core';
+import { WebsocketExceptionFilter } from '../common/filter/websocket.exception.filter';
 
 @Module({
-  imports: [GameRoomModule, GameModule,],
-  providers: [EventGateway, EventManager],
+  imports: [GameRoomModule, GameModule],
+  providers: [EventGateway, EventManager, {
+    provide: APP_FILTER,
+    useClass: WebsocketExceptionFilter,
+  }],
 })
-export class EventModule {}
+export class EventModule {
+}
