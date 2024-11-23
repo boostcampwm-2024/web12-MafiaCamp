03a5c652c66df20669e5cbc14e872f386471218e
HyunJinNo <nhj9831@naver.com>
2024-11-23 21:47:18 +0900

    fix: 빌드 오류 수정

--- FE/src/app/login/kakao/callback/page.tsx
+++ FE/src/app/login/kakao/callback/page.tsx
@@ -1,5 +1,10 @@
 import AuthLoading from '@/components/signin/loading/AuthLoading';
+import { Suspense } from 'react';
 
 export default function page() {
-  return <AuthLoading />;
+  return (
+    <Suspense>
+      <AuthLoading />
+    </Suspense>
+  );
 }
