131ae1df16eeaba2535ecdcbb8f8da27122b0005
HyunJinNo <nhj9831@naver.com>
2024-11-27 21:34:30 +0900

    remove: 로비 페이지 닉네임 설정 모달 컴포넌트 삭제

--- FE/src/components/lobby/LobbyViewer.tsx
+++ FE/src/components/lobby/LobbyViewer.tsx
@@ -3,45 +3,35 @@
 import { io } from 'socket.io-client';
 import LobbyBanner from './LobbyBanner';
 import LobbyList from './LobbyList';
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import { useSocketStore } from '@/stores/socketStore';
-import NicknameModal from './NicknameModal';
 import { useRouter } from 'next/navigation';
 import { useAuthStore } from '@/stores/authStore';
 import ConnectedUserList from './ConnectedUserList';
 
 const LobbyViewer = () => {
-  const { userId, nickname } = useAuthStore();
+  const { userId } = useAuthStore();
   const { setSocketState } = useSocketStore();
-  const [hasNickname, setHasNickname] = useState(false);
   const router = useRouter();
 
   useEffect(() => {
-    if (hasNickname) {
-      const socket = io(`${process.env.NEXT_PUBLIC_BACKEND_URL}/ws`, {
-        transports: ['websocket', 'polling'], // use WebSocket first, if available
-      });
+    const socket = io(`${process.env.NEXT_PUBLIC_BACKEND_URL}/ws`, {
+      transports: ['websocket', 'polling'], // use WebSocket first, if available
+    });
 
-      socket.on('connect_error', (error) => {
-        console.error(`연결 실패: ${error}`);
-        alert('서버와의 연결에 실패하였습니다. 잠시 후에 다시 시도해 주세요.');
-        socket.disconnect();
-        setSocketState({ socket: null });
-        router.replace('/');
-      });
+    socket.on('connect_error', (error) => {
+      console.error(`연결 실패: ${error}`);
+      alert('서버와의 연결에 실패하였습니다. 잠시 후에 다시 시도해 주세요.');
+      socket.disconnect();
+      setSocketState({ socket: null });
+      router.replace('/');
+    });
 
-      setSocketState({ socket });
-      socket.emit('set-nickname', { nickname });
-    }
-
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [hasNickname, router, setSocketState]);
+    setSocketState({ socket });
+  }, [router, setSocketState]);
 
   return (
     <div className='flex flex-col items-center'>
-      {!hasNickname && (
-        <NicknameModal setHasNickname={() => setHasNickname(true)} />
-      )}
       {userId !== '' && (
         <ConnectedUserList
           userList={[
