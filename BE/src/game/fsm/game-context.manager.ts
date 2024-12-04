3b6fe007ab6201324a856257999b13f53f7b92a2
0Chord <seaweed.0chord@gmail.com>
2024-12-04 19:10:12 +0900

    fix: gameContext 중복 제거

--- BE/src/game/fsm/game-context.manager.ts
+++ BE/src/game/fsm/game-context.manager.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { GameContext } from './game-context';
 import { LockManager } from '../../common/utils/lock-manager';
+import { DuplicateGameContextException } from '../../common/error/duplicate.game-context.exception';
 
 @Injectable()
 export class GameContextManager {
@@ -8,20 +9,23 @@ export class GameContextManager {
   private readonly gameContexts = new Map<string, GameContext>();
 
   async setContext(roomId: string, context: GameContext) {
-    return await this.lockManager.withKeyLock(roomId,async ()=>{
+    return await this.lockManager.withKeyLock(roomId, async () => {
+      if (this.gameContexts.has(roomId)) {
+        throw new DuplicateGameContextException();
+      }
       this.gameContexts.set(roomId, context);
-    })
+    });
   }
 
   async getContext(roomId: string): Promise<GameContext | undefined> {
-    return await this.lockManager.withKeyLock(roomId,async ()=>{
+    return await this.lockManager.withKeyLock(roomId, async () => {
       return this.gameContexts.get(roomId);
-    })
+    });
   }
 
   async removeContext(roomId: string) {
-    return await this.lockManager.withKeyLock(roomId, async ()=>{
+    return await this.lockManager.withKeyLock(roomId, async () => {
       this.gameContexts.delete(roomId);
-    })
+    });
   }
 }
